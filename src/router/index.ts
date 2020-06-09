b9fc861b339719c2c33a7ea944fe12cc37245deb
zhangjianlei <djkloopjs@gmail.com>
2020-06-09 17:21:07 +0800

    first commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,7 @@
-import Vue from "vue";
-import VueRouter, { RouteConfig } from "vue-router";
+import { RouteRecordRaw, createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
 
-Vue.use(VueRouter);
-
-const routes: Array<RouteConfig> = [
+const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
@@ -16,12 +13,12 @@ const routes: Array<RouteConfig> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: () => import(/* webpackChunkName: "about" */ "../views/About")
   }
 ];
 
-const router = new VueRouter({
+const router = createRouter({
+  history: createWebHashHistory(),
   routes
 });
 
---
+++ src/views/About.tsx
@@ -0,0 +1,8 @@
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "About",
+  setup() {
+    return () => <div>about</div>;
+  }
+});
